c0372c13217f4ec93b0e7c1ed7d7edf02127e990
saasbook <saasbook@saasbook.(none)>
2013-10-30 03:06:47 -0700

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rottenpotatoes::Application.routes.draw do
-	 resources :movies
- 	 root :to => redirect('/movies')
-	
+		
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
